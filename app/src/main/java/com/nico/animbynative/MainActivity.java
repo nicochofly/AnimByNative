124ac01328545e7f27fc220c886ea36d203e117d
nicocho <caodq1@163.com>
2022-04-11 22:32:18 +0800

    上传图片，跑通demo

--- app/src/main/java/com/nico/animbynative/MainActivity.java
+++ app/src/main/java/com/nico/animbynative/MainActivity.java
@@ -1,35 +1,83 @@
 package com.nico.animbynative;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements AssertReleaseTask.ReleaseCallback {
-    private SurfaceView mSurfaceView;
+    private SurfaceView mSurfaceView = null;
+    int pos = 0;
+    String[] pngs = {"frame0.png", "frame1.png",
+            "frame2.png",
+            "frame3.png",
+            "frame4.png",
+            "frame5.png",
+            "frame6.png",
+            "frame7.png",
+            "frame8.png",
+            "frame9.png",
+            "frame10.png",
+            "frame11.png",
+            "frame12.png",
+            "frame13.png",
+            "frame14.png",
+            "frame15.png",
+            "frame16.png",
+            "frame17.png",
+            "frame18.png",
+            "frame19.png"};
+    private boolean isPlaying = false;
+    private Surface surface = null;
+    private PngLoader pngLoader = null;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            if (msg.what == 1) {
+                if (pos >= pngs.length) {
+                    pos = 0;
+                }
+                String path = getExternalFilesDir(null).getAbsolutePath() + File.separator + pngs[pos];
+                pngLoader.loadPNGImage(path, surface);
+                pos += 1;
+            }
+
+
+            Message message = obtainMessage();
+            message.what = 1;
+            message.obj = surface;
+            sendMessageDelayed(message, 30);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setupView();
-    }
 
-
-    private void setupView() {
+        pngLoader = new PngLoader();
         mSurfaceView = new SurfaceView(this);
-        setContentView(mSurfaceView);
+        ViewGroup.LayoutParams parms = new ViewGroup.LayoutParams(1024, 1024);
+        mSurfaceView.setLayoutParams(parms);
         mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                PngLoader nativeImageLoader = new PngLoader();
-                File file = new File(getExternalFilesDir(null), "input.png");
-                nativeImageLoader.loadPNGImage(file.getAbsolutePath(), holder.getSurface());
+
+                surface = holder.getSurface();
+                Message msg = mHandler.obtainMessage();
+                msg.what = 1;
+                msg.obj = surface;
+                mHandler.sendMessageDelayed(msg, 1000);
             }
 
             @Override
@@ -42,20 +90,30 @@ public class MainActivity extends AppCompatActivity implements AssertReleaseTask
 
             }
         });
+        mSurfaceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    isPlaying = false;
+                    mHandler.removeCallbacksAndMessages(null);
+                } else {
+                    Message msg = mHandler.obtainMessage();
+                    msg.what= 1;
+                    msg.obj = surface;
+                    mHandler.sendMessageDelayed(msg, 100);
+                }
+            }
+        });
+        setContentView(mSurfaceView);
     }
-//    @Override
-//    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        AssertReleaseTask task = new AssertReleaseTask(this, "input.png", this);
-//        AsyncTaskCompat.executeParallel(task);
-//    }
+
+
 
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        AssertReleaseTask task = new AssertReleaseTask(this, "input.png", this);
-//        AsyncTaskCompat.executeParallel(task);
+        AssertReleaseTask task = new AssertReleaseTask(this, pngs, this);
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
